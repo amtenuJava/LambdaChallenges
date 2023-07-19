6f5ecbe5f7ca80a8ae3f959872a6ef98138352b8
amanuel <amannov21@gmail.com>
2023-07-19 14:59:33 -0600

    get a random char from start to end making end inclusive and cast to char to out put a char.
    Transform the list adding random initial

--- src/com/amanuel/LambdaExpressionChallenge.java
+++ src/com/amanuel/LambdaExpressionChallenge.java
@@ -5,7 +5,7 @@ Include one name which can be read the same backwards and forward (bob)
  -> Transform names all to upper case
  -> Add a randomly generated middle initial, and include a period.
  -> add a last name which is a reverse of the first name.
- ->Print the array after each change
+ -> Print the array after each change
  -> Create a new modifiable ArrayList removing names where the first name is equal to the last name
 
  */
@@ -28,7 +28,16 @@ public class LambdaExpressionChallenge {
 
         System.out.println("Transformed to upper case "+ Arrays.toString(names ));
 
-        List<String> namesList=new ArrayList<>(Arrays.asList(names));
-        System.out.println();
+        List<String> namesList=Arrays.asList(names);
+
+        namesList.replaceAll(s -> s += " " +getRandomAlphabet('A','E')+ ".");
+        System.out.println("+++ Initials Added +++++++");
+        System.out.println(Arrays.toString(names));
+
+    }
+
+
+    public static char getRandomAlphabet(char start,char end){
+        return (char) random.nextInt((int)start,(int)end+1 );
     }
 }
